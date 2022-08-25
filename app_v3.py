e9a351bc417b47e75c733743991269c473ee1207
Dhinson Dacpano <dhinson.dacpano@gmail.com>
2022-08-25 22:21:30 +0200

    set log filename date on end of day

--- app_v3.py
+++ app_v3.py
@@ -13,16 +13,16 @@ def main():
     with open('./config_v3.yaml','r') as f:
         config = yaml.load(f.read(), Loader = yaml.FullLoader)
 
-    config['logging']['handlers']['file']['filename'] = date.today().strftime('%y-%m-%d') + '_bot_log.log'
-
     logging.addLevelName(FILE,"FILE")
-    logging.config.dictConfig(config['logging'])
-
+    
     # arbitrage_strat = check_riskfree_trade_v2
     # arbitrage_strat = collar_strategy
     arbitrage_strat = selling_premiums
 
     while True:
+        config['logging']['handlers']['file']['filename'] = date.today().strftime('%y-%m-%d') + '_bot_log.log'
+        logging.config.dictConfig(config['logging'])
+
         deribit_exch = Deribit_Exchange(**config['exchange'])
         bot = CBot(**config['bot'], exchange=deribit_exch, arbitrage_strategy=arbitrage_strat, money_mngmt=None)
         bot.run()
