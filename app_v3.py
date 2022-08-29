38e1182bc946cff224fe110bced38971ef755cd7
Dhinson Dacpano <dhinson.dacpano@gmail.com>
2022-08-29 19:57:37 +0200

    fixes

--- app_v3.py
+++ app_v3.py
@@ -18,6 +18,10 @@ def main():
     logging.addLevelName(FILE,"FILE")
     # config['logging']['handlers']['file']['filename'] = logfile
     logging.config.dictConfig(config['logging'])
+    logger = logging.getLogger(__name__)
+    for logr in logger:
+        if isinstance(logr, logging.handlers.TimedRotatingFileHandler):
+            logr.suffix = '%y-%m-%d_%H_%M_bot_log.csv'
     
 
     # logging.basicConfig(filename=logfile)
@@ -26,8 +30,8 @@ def main():
     # arbitrage_strat = collar_strategy
     arbitrage_strat = selling_premiums
     
-    deribit_exch = Deribit_Exchange(**config['exchange'])
-    bot = CBot(**config['bot'], exchange=deribit_exch, arbitrage_strategy=arbitrage_strat, money_mngmt=None)
+    deribit_exch = Deribit_Exchange(**config['exchange'], logger=logger)
+    bot = CBot(**config['bot'], exchange=deribit_exch, arbitrage_strategy=arbitrage_strat, money_mngmt=None, logger=logger)
     bot.run()
 
     # while True:
--- exchange.py
+++ exchange.py
@@ -27,7 +27,6 @@ class Deribit_Exchange:
 
         if self.logger is None:
             self.logger = logging.getLogger(__name__)
-            self.logger.suffix = '%y-%m-%d_%H_%M_bot_log.csv'
 
         self.df_initcols = ['strike', 'instrument_name', 'option_type']
 
